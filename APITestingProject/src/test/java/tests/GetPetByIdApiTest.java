fe7612c82c93b8927957d940b07d5739a2a63bce
Diana Gall <dianagall47@gmail.com>
2024-08-01 14:31:21 +0100

    add test for get pet by invalid id

--- APITestingProject/src/test/java/tests/GetPetByIdApiTest.java
+++ APITestingProject/src/test/java/tests/GetPetByIdApiTest.java
@@ -1,10 +1,11 @@
 package tests;
 
-import org.junit.jupiter.api.BeforeAll;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.Matchers.*;
 
 public class GetPetByIdApiTest {
@@ -36,6 +37,21 @@ public class GetPetByIdApiTest {
                     .extract().response();
 
         }
+
+    @Test
+    @DisplayName("expect 500 Error processing request when searching for an invalid id")
+    public void testGetPetByInvalidId() {
+        int invalidPetId = -1;
+
+        RestAssured
+                .given()
+                .spec(Utils.getPetByIdRequestSpec(baseUri, petEndpoint, invalidPetId))
+                .when()
+                .get()
+                .then()
+                .statusCode(500)
+                .body("message", containsString("There was an error processing your request. It has been logged"));
+    }
 
 
 }
